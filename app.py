67e618867d91602542d702a1c09ea9bab5d7c110
Jacob Valdez <jacobdabdez@icloud.com>
2023-02-12 14:23:00 -0600

    Part 2 + fixes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
@@ -18,21 +18,17 @@ db.create_all()
 @app.route('/')
 def root():
     """Homepage for Blogly App"""
+    users = User.query.all()
+    posts = Post.recent_posts()
+    return render_template('index.html', users=users, posts=posts)
 
-    return redirect("/users")
 
-
-@app.route('/users')
+@app.route('/users/new')
 def users_index():
+    """Page to create Users"""
 
     users = User.query.order_by(User.last_name, User.first_name).all()
-    return render_template('users/index.html', users=users)
-
-
-@app.route('/users/new', methods=["GET"])
-def users_new_form():
-
-    return render_template('users/new.html')
+    return render_template('newUser.html', users=users)
 
 
 @app.route("/users/new", methods=["POST"])
@@ -47,26 +43,27 @@ def users_new():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect("/users")
+    return redirect("/")
 
 
 @app.route('/users/<int:user_id>')
 def users_show(user_id):
+    """Shows user details"""
 
     user = User.query.get_or_404(user_id)
-    return render_template('users/show.html', user=user)
+    return render_template('details.html', user=user)
 
 
-@app.route('/users/<int:user_id>/edit')
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
 def users_edit(user_id):
-
+    """edits user info form"""
     user = User.query.get_or_404(user_id)
-    return render_template('users/edit.html', user=user)
+    return render_template('editUser.html', user=user)
 
 
 @app.route('/users/<int:user_id>/edit', methods=["POST"])
 def users_update(user_id):
-
+    """Updates user info in db, redirects to home page"""
     user = User.query.get_or_404(user_id)
     user.first_name = request.form['first_name']
     user.last_name = request.form['last_name']
@@ -75,14 +72,80 @@ def users_update(user_id):
     db.session.add(user)
     db.session.commit()
 
-    return redirect("/users")
+    return redirect(f'/users/{user.id}')
 
 
 @app.route('/users/<int:user_id>/delete', methods=["POST"])
 def users_destroy(user_id):
-
+    """Destroys the user from the db"""
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
 
-    return redirect("/users")
+    return redirect("/")
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def show_new_post_form(user_id):
+    """Adds post form for selected user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('newPost.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def submit_new_post(user_id):
+    """Submit new post and redirect to user details page"""
+
+    title = request.form['title']
+    content = request.form['content']
+
+    post = Post(title=title, content=content, user_id=user_id)
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f'/users/{user_id}')
+
+
+@app.route('/posts/<int:post_id>')
+def display_post(post_id):
+    """Displays post of given post_id"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post_form(post_id):
+    """Display edit post form"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('editPost.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def edit_post(post_id):
+    """Edits post and redirect back to post page"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f'/posts/{post.id}')
+
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    """Delete post and return to user page"""
+
+    post = Post.query.get_or_404(post_id)
+    user_id = post.user.id
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f'/users/{user_id}')
--- models.py
+++ models.py
@@ -1,12 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from dateutil import tz
 
 db = SQLAlchemy()
 
 DEFAULT_IMAGE_URL = "https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg"
 
 
-class User(db.Model):
+def connect_db(app):
+    """Connect to app to database"""
+    db.app = app
+    db.init_app(app)
 
+
+class User(db.Model):
+    """SQLAlchemy Users model"""
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -17,10 +25,51 @@ class User(db.Model):
     @property
     def full_name(self):
 
-        return f"{self.first_name} {self.last_name}"
+        if self.last_name:
+            return self.first_name + ' ' + self.last_name
+        else:
+            return self.first_name
 
 
-def connect_db(app):
+class Post(db.Model):
+    """SQLAlchemy posts model"""
 
-    db.app = app
-    db.init_app(app)
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    title = db.Column(db.String(40),
+                      nullable=False)
+
+    content = db.Column(db.String(),
+                        nullable=False)
+
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id', ondelete='CASCADE'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref(
+        'posts', cascade='all, delete-orphan'))
+
+    def convert_created_at(self):
+        f = "%b %d %Y %r"
+
+        timestamp = self.created_at
+
+        from_zone = tz.tzutc()
+        to_zone = tz.tzlocal()
+
+        timestamp = timestamp.replace(tzinfo=from_zone)
+        timestamp = timestamp.astimezone(to_zone)
+
+        return timestamp.strftime(f)
+
+    @classmethod
+    def recent_posts(cls):
+        """Returns 5 most recent posts"""
+
+        return cls.query.order_by(Post.created_at.desc()).limit(5).all()
